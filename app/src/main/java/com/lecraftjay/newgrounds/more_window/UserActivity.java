9d13195a43642e5ef23333d7026ebe7398856edd
Jason <jm.enns04@gmail.com>
2022-05-12 10:57:20 +0200

    the last change

---
+++ app/src/main/java/com/lecraftjay/newgrounds/more_window/UserActivity.java
@@ -0,0 +1,16 @@
+package com.lecraftjay.newgrounds.more_window;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.lecraftjay.newgrounds.R;
+
+public class UserActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user);
+    }
+}
